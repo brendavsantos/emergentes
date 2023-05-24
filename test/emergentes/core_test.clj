aaff17c3fceef132562bc9026978d0c054e0f5d7
Brenda Santos <brendasantosv.bs@gmail.com>
2023-05-24 20:16:05 -0300

    Adicionando duplicacoes nos testes

--- test/emergentes/core_test.clj
+++ test/emergentes/core_test.clj
@@ -12,7 +12,17 @@
 
 (deftest incompleteAuthor-test
   (testing "Verificar a completude de um autor com campos faltando"
-    (is (= 1.0 (completeness-author {:birthCountry "Brasil"} 5.0)))))
+    (is (= 3.0 (completeness-author {:birthCountry "Brasil"
+                                     :birthCity "Porto Alegre"
+                                     :birthState "Goias"} 5.0)))
+    (is (= 2.0 (completeness-author {:birthCountry "Brasil"
+                                     :identifier.lattes "310369"} 5.0)))
+    (is (= 2.0 (completeness-author {:birthCountry "Brasil"
+                                     :identifier.orcid "130325"} 5.0)))
+    (is (= 1.0 (completeness-author {:birthCity "Gama" } 5.0)))
+    (is (= 3.0 (completeness-author {:birthCountry "Brasil"
+                                     :nationality "Brasileira"
+                                     :identifier.orcid "677232"} 5.0)))))
 
 (deftest correctIdentifier-test
   (testing "Verificar a completude só com um identificador"
@@ -21,11 +31,14 @@
 
 (deftest wrongIdentifier-test
   (testing "Verificar a completude usando os dois identificadores ou nenhum"
-    (is (= 0 (completeness-identifier {:name "Helena"} 5.0)))
+    (is (= 0 (completeness-identifier {:name "Helena"} 5.0))) 
+    (is (= 0 (completeness-identifier nil 5.0)))
     (is (= 0 (completeness-identifier {:identifier.lattes "1156310"
-                                       :identifier.orcid "37023"} 5.0))))) 
+                                       :identifier.orcid "37023"} 5.0)))
+    (is (= 5.0 (completeness-identifier {:identifier.lattes "1156310"} 5.0))))) 
 
-(run-tests *ns*)
+
+(run-tests *ns*) 
 (completeAuthor-test)
 (incompleteAuthor-test)
 (correctIdentifier-test)
