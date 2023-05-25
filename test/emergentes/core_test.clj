5491eb6ac62b6b9e4f7da145e231bc24484a8da2
Brenda Santos <brendasantosv.bs@gmail.com>
2023-05-24 22:42:18 -0300

    Adicionando triangulacao nos testes

--- test/emergentes/core_test.clj
+++ test/emergentes/core_test.clj
@@ -2,45 +2,96 @@
   (:require [clojure.test :refer :all]
             [emergentes.core :refer :all]))
 
-(deftest completeAuthor-test
-  (testing "Verificar a completude de um autor com todos os campos preenchidos"
-    (is (= 5.0 (completeness-author {:nationality "Brasileiro"
-                                     :birthCountry "Brasil"
-                                     :birthCity "Sao Paulo"
-                                     :birthState "SP"
-                                     :identifier.lattes "502205"} 5.0)))))
+  (let [test-cases [{:author {:nationality "Brasileiro"
+                              :birthCountry "Brasil"
+                              :birthCity "Sao Paulo"
+                              :birthState "SP"
+                              :identifier.lattes "502205"}
+                     :weight 5.0}
 
-(deftest incompleteAuthor-test
-  (testing "Verificar a completude de um autor com campos faltando"
-    (is (= 3.0 (completeness-author {:birthCountry "Brasil"
-                                     :birthCity "Porto Alegre"
-                                     :birthState "Goias"} 5.0)))
-    (is (= 2.0 (completeness-author {:birthCountry "Brasil"
-                                     :identifier.lattes "310369"} 5.0)))
-    (is (= 2.0 (completeness-author {:birthCountry "Brasil"
-                                     :identifier.orcid "130325"} 5.0)))
-    (is (= 1.0 (completeness-author {:birthCity "Gama" } 5.0)))
-    (is (= 3.0 (completeness-author {:birthCountry "Brasil"
-                                     :nationality "Brasileira"
-                                     :identifier.orcid "677232"} 5.0)))))
+                    {:author {:nationality "Americano"
+                              :birthCountry "USA"
+                              :birthCity "New York"
+                              :birthState "NY"
+                              :identifier.lattes "123456"}
+                     :weight 4.0}
+                    {:author {:nationality "Alemao"
+                              :birthCountry "Alemanha"
+                              :birthCity "Berlin"
+                              :birthState "Brandemburgo"
+                              :identifier.lattes "654139"}
+                     :weight 4.0}
+                    {:author {:nationality "Colombiano"
+                              :birthCountry "Colombia"
+                              :birthCity "Bogota"
+                              :birthState "Cundinamarca"
+                              :identifier.lattes "402902"}
+                     :weight 4.0}
+                    ]]
+    
+    (doseq [test-case test-cases]
+      (let [author (:author test-case)
+            weight (:weight test-case)]
+        (testing (str "Verificar a completude de um autor com parâmetros: " author " e peso: " weight)
+          (is (= weight (completeness-author author weight))))))) 
 
-(deftest correctIdentifier-test
-  (testing "Verificar a completude só com um identificador"
-    (is (= 5.0 (completeness-identifier {:identifier.lattes "3035015"} 5.0)))
-    (is (= 5.0 (completeness-identifier {:identifier.orcid "91073"} 5.0)))))
+(let [test-cases [{:author {:birthCountry "Brasil"
+                             :birthCity "Porto Alegre"
+                             :birthState "Goias"}
+                     :weight 5.0}
+                    
+                    {:author {:birthCountry "Brasil"
+                             :identifier.lattes "310369"}
+                     :weight 5.0}
+                    
+                    {:author {:birthCountry "Brasil"
+                             :identifier.orcid "130325"}
+                     :weight 5.0}
+                    
+                    {:author {:birthCity "Gama"}
+                     :weight 5.0}
+                    
+                    {:author {:birthCountry "Brasil"
+                             :nationality "Brasileira"
+                             :identifier.orcid "677232"}
+                     :weight 5.0} 
+                    ]]
+    (doseq [test-case test-cases]
+      (let [author (:author test-case)
+            weight (:weight test-case)]
+        (testing (str "Verificar a completude de um autor com parâmetros: " author " e peso: " weight)
+          (is (= weight (completeness-author author weight)))))))
 
-(deftest wrongIdentifier-test
-  (testing "Verificar a completude usando os dois identificadores ou nenhum"
-    (is (= 0 (completeness-identifier {:name "Helena"} 5.0))) 
-    (is (= 0 (completeness-identifier nil 5.0)))
-    (is (= 0 (completeness-identifier {:identifier.lattes "1156310"
-                                       :identifier.orcid "37023"} 5.0)))
-    (is (= 5.0 (completeness-identifier {:identifier.lattes "1156310"} 5.0))))) 
+(let [test-cases [{:author {:identifier.lattes "3035015"}
+                     :weight 5.0}
+                    
+                    {:author {:identifier.orcid "91073"}
+                     :weight 5.0} 
+                    ]]
+    (doseq [test-case test-cases]
+      (let [author (:author test-case)
+            weight (:weight test-case)]
+        (testing (str "Verificar a completude só com um identificador: " author " e peso: " weight)
+          (is (= weight (completeness-identifier author weight)))))))
 
+(let [test-cases [{:author {:name "Helena"}
+                   :weight 5.0}
+
+                  {:author nil
+                   :weight 5.0}
+
+                  {:author {:identifier.lattes "1156310"
+                            :identifier.orcid "37023"}
+                   :weight 5.0}
+
+                  {:author {:identifier.lattes "1156310"}
+                   :weight 5.0} 
+                  ]]
+  (doseq [test-case test-cases]
+    (let [author (:author test-case)
+          weight (:weight test-case)]
+      (testing (str "Verificar a completude usando os identificadores: " author " e peso: " weight)
+        (is (= weight (completeness-identifier author weight))))))) 
 
 (run-tests *ns*) 
-(completeAuthor-test)
-(incompleteAuthor-test)
-(correctIdentifier-test)
-(wrongIdentifier-test)
 
